ef6657e1924e1c2ea7d5ef22817853fe765ef9f9
huongpx <huong.px01@gmail.com>
2022-07-13 00:14:28 +0700

    add cors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask
 from flask_migrate import Migrate
 from flask_restful import Api
+from flask_cors import CORS
 
 from models import db
 from resources import TodoResource, TodoDetailResource
 from auth import auth_user
 
 app = Flask(__name__)
+CORS(app)
 app.config.from_object('config')
 
 db.init_app(app)
